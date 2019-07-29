64c561d645a2c3c8448ba5ac0e8edb2d31f303eb
David Mills <dave@webshed.org>
2019-07-29 14:07:37 +0100

    NOOOOOO this really is a awful way to get the base address of the array DO NOT USE\!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
     //  Created by David Mills on 25/07/2019.
     //  Test code to see if boost_multiarray will be easier to use / develop with than the current code
     //
-
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
@@ -28,6 +28,19 @@ struct thead
     // TOMfile format is 512 bytes header as descibed by the struct
     // followed by xsize x ysize x zsize of uint8_t 's
     // optional for there to be a trailer after this, but that's from old version of format and is now ignored
+
+struct MyAllocator : public std::allocator<uint8_t> {
+    uint8_t *allocate(size_t n, const void *hint = 0) {
+        uint8_t *result = allocator::allocate(n,hint);
+        printf("                                        allocate(%zu,%p) -> %p\n", n, hint, result);
+        return result;
+    }
+
+    void deallocate(uint8_t *p, size_t n) {
+        allocator::deallocate(p, n);
+        printf("                                        deallocate(%p,%zu)\n", p, n);
+    }
+};
 
 
 int main(int argc, const char * argv[]) {
@@ -60,50 +73,87 @@ int main(int argc, const char * argv[]) {
         }
 
 
+    boost::multi_array<uint8_t,3,MyAllocator> TomVolume;
+    printf("volume.data() == %p before resize\n", TomVolume.data());
+    printf("volume.origin() == %p before resize\n", TomVolume.origin());
+
+    TomVolume.resize(boost::extents[TomHeader.xsize][TomHeader.ysize][TomHeader.zsize]);
+    printf("volume.data() == %p after resize\n", TomVolume.data());
+    printf("volume.origin() == %p after resize\n", TomVolume.origin());
 
         // create array to hold the image data
         // Create a 3D array that is xsize x ysize x zsize
 
-    boost::multi_array<uint8_t,3> TomVolume;
-        //TomVolume.resize(boost::extents[20][20][20]);
-    TomVolume.resize(boost::extents[TomHeader.xsize][TomHeader.ysize][TomHeader.zsize]);
+    //boost::multi_array<uint8_t,3> TomVolume;
+    //TomVolume.resize(boost::extents[20][20][20]);
+    //TomVolume.resize(boost::extents[TomHeader.xsize][TomHeader.ysize][TomHeader.zsize]);
 
+    char *end;
+    char *buf;
+    size_t sz;
+    sz = snprintf(NULL, 0, "%p", TomVolume.data());
+    buf = (char *)malloc(sz + 1); /* make sure you check for != NULL in real code */
+    snprintf(buf, sz+1, "%p", TomVolume.data());
+    size_t data = std::strtoul(buf, &end, 16);
+        //size_t data = atoi(buf);
 
         // get handle to where the data actually lives and how much space we have allocated
-    uint8_t *data = TomVolume.origin();
-        //size_t bytes = 8000;//TomHeader.xsize * TomHeader.ysize * TomHeader.zsize;
+        //size_t wibble = TomVolume.data();
+        //size_t *data = TomVolume.origin();
+    //size_t bytes = 8000;//TomHeader.xsize * TomHeader.ysize * TomHeader.zsize;
     size_t bytes = TomHeader.xsize * TomHeader.ysize * TomHeader.zsize;
     tomfile.seekg(512); // skip the file header
-    tomfile.read((char*) &data, bytes);
+    tomfile.read((char*) *&data, bytes);
     tomfile.close();
+
+
 
 
         // make a RGB slice - set R=G=B
         // totally not optimal, but will do for testing.
         // write out a PNG that's just the central XY slice from the Z stack
-
+    std::cout << "Loaded data and closed file" << std::endl;
     std::vector<uint8_t> rgbslice;
     rgbslice.resize(3 * TomHeader.xsize * TomHeader.ysize);
-    for(unsigned int rgb_pixel = 0; rgb_pixel < rgbslice.size(); rgb_pixel++)
-        for(unsigned int i = 0; i < TomHeader.xsize; i++)
-            for(unsigned int j = 0; j < TomHeader.ysize; j++)
-                {
-                    //red
-                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
-                rgb_pixel++;
-                    //green
-                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
-                rgb_pixel++;
-                    //blue
-                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
-                }
+    uint8_t *rgb_pixel = rgbslice.data();
+    for(unsigned int i = 0; i < TomHeader.xsize; i++)
+        for(unsigned int j = 0; j < TomHeader.ysize; j++)
+            {
+                //red
+            *rgb_pixel++ = TomVolume[i][j][TomHeader.zsize / 2];
+                //green
+            *rgb_pixel++ = TomVolume[i][j][TomHeader.zsize / 2];
+                //blue
+            *rgb_pixel++ = TomVolume[i][j][TomHeader.zsize / 2];
+            }
 
+    std::cout << "Made a slice" << std::endl;
+
+
+//    for(unsigned int rgb_pixel = 0; rgb_pixel < rgbslice.size(); rgb_pixel++)
+//        for(unsigned int i = 0; i < TomHeader.xsize; i++)
+//            for(unsigned int j = 0; j < TomHeader.ysize; j++)
+//                {
+//                    //red
+//                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
+//                rgb_pixel++;
+//                    //green
+//                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
+//                rgb_pixel++;
+//                    //blues
+//                rgbslice[rgb_pixel] = TomVolume[i][j][TomHeader.zsize / 2];
+//                }
+//
     uint8_t* p = rgbslice.data();
-
+//
     std::ofstream out("slice.png", std::ios::binary);
     TinyPngOut pngout(static_cast<uint32_t>(TomHeader.xsize), static_cast<uint32_t>(TomHeader.ysize), out);
-
+//
     pngout.write(p, static_cast<size_t>(TomHeader.xsize * TomHeader.ysize));
-    
+    std::cout << "Wrote out slice" << std::endl;
     return 0;
 }
+
+
+    ///   export LDFLAGS="-L/usr/local/opt/boost@1.60/lib"
+    ///   export CPPFLAGS="-I/usr/local/opt/boost@1.60/include"
